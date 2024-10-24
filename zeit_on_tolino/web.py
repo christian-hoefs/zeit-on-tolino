0097591810a14a4a420fec9482f49a530061f22e
christian-hoefs <91870169+christian-hoefs@users.noreply.github.com>
2024-10-24 18:20:05 +0200

    Update web.py

--- zeit_on_tolino/web.py
+++ zeit_on_tolino/web.py
@@ -12,10 +12,7 @@ from selenium.webdriver.firefox.webdriver import WebDriver
 DOWNLOAD_PATH = tempfile.TemporaryDirectory().name
 
 user_agents = [
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.199 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"
 ]
 
 @dataclass
